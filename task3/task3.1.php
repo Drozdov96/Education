82f98487e447201cb8a8a4138de50383a92806fe
Drozdov96 <drozdov.na96@gmail.com>
2018-11-01 19:52:43 +0400

    Split code to the functions and debug.

--- task3/task3.1.php
+++ task3/task3.1.php
@@ -4,10 +4,9 @@ $temperatureToday = 12;
 $temperatureTomorrow = 6;
 $temperatureYesterday = 13;
 $isRaining = false;
-$phraseFromAnya = "холодно b и ызамерзла заморозки";
+$phraseFromAnya = "b и ызамерзла заморозки";
 
-echo momsThinkingProcess($temperatureToday, $temperatureTomorrow, $temperatureYesterday,
-							 $isRaining, $phraseFromAnya);
+echo momsThinkingProcess($temperatureToday, $temperatureTomorrow, $temperatureYesterday, $isRaining, $phraseFromAnya);
 
 
 
@@ -20,39 +19,35 @@ function momsThinkingProcess(int $temperatureToday, int $temperatureTomorrow, in
     $coldPhrasesNumber=analyzeAnyaPhrase($phraseFromAnya);
 
     if(isDanger($isTemperatureFall, $coldPhrasesNumber,
-        rateTemperatureDrop($temperatureToday, $temperatureTomorrow, 5)))
+        rateTemperatureDropTomorrow($temperatureToday, $temperatureTomorrow, 5)))
     {
 		$momsIncomingSentence="*Сказано голосом Гендальфа* Ты не пройдёшь!";
 	}else{
 		
 		if($temperatureToday<13) {
-			if($temperatureYesterday>=11 && $temperatureTomorrow>=11) {
+			if($temperatureYesterday>=11 && $temperatureTomorrow>=11)
+			{
 				$momsIncomingSentence="Одень шапку. ";
-			}elseif($temperatureYesterday<11 || $temperatureTomorrow<11) {
+			}else{
 				$momsIncomingSentence="Одень зимнюю шапку. ";
 			}
-		}
-		
-		if(($temperatureYesterday>$temperatureToday && $temperatureToday>$temperatureTomorrow) || 
-		   (substr_count($phraseFromAnya, "холодно")>0 || substr_count($phraseFromAnya, "заморозки")>0 ||
-		   substr_count($phraseFromAnya, "замерзла")>0)) {
-			$momsIncomingSentence.="ты хорошо оделся? ";
+		}elseif($isTemperatureFall || $coldPhrasesNumber > 0) {
+			$momsIncomingSentence="Ты хорошо оделся? ";
 		}
 		
 		if($isRaining) {
-			$momsIncomingSentence.="и возьми с собой зонтик. ";
+			$momsIncomingSentence.="И возьми с собой зонтик. ";
 			
-			if(($temperatureToday-$temperatureTomorrow)>3) {
-				$momsIncomingSentence.="и шарф.";
+			if(rateTemperatureDropTomorrow($temperatureToday, $temperatureTomorrow, 3)) {
+				$momsIncomingSentence.="И шарф.";
 			}
 		}
-	
 	}
 	return $momsIncomingSentence;
 }
 
 function isDanger(bool $isTemperatureFall, int $coldPhrasesNum, bool $isTempDropOnFiveUnits){
-    return  ($coldPhrasesNum > 2)&& $isTemperatureFall && $isTempDropOnFiveUnits;
+    return  ($coldPhrasesNum >= 2)&& $isTemperatureFall && $isTempDropOnFiveUnits;
 }
 
 function analyzeAnyaPhrase(string $phraseFromAnya)
@@ -72,7 +67,7 @@ function checkTemperatureFall(int $temperatureYesterday, int $temperatureToday, 
     return $temperatureYesterday>$temperatureToday && $temperatureToday>$temperatureTomorrow;
 }
 
-function rateTemperatureDrop(int $temperatureToday, int $temperatureTomorrow, int $temperatureDropNumber)
+function rateTemperatureDropTomorrow(int $temperatureToday, int $temperatureTomorrow, int $temperatureDropNumber)
 {
     return ($temperatureToday-$temperatureTomorrow)>$temperatureDropNumber;
 }
