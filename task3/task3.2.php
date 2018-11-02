d7a52146bfc98eeec8caca77ae139d5379f1da6c
Drozdov96 <drozdov.na96@gmail.com>
2018-11-02 15:55:08 +0400

    Working on few mistakes.

--- task3/task3.2.php
+++ task3/task3.2.php
@@ -1,59 +1,57 @@
 <?php
 
-$result=whoIsWinner(array('6:f'=>'белый:слон', '5:f'=>'белый:слон', '2:g'=>'белый:король', '7:f'=>'черный:конь',
-    '8:g'=>'черный:слон', '8:h'=>'черный:король'));
+$result=whoIsWinner(array('6:f'=>'черный:слон', '5:f'=>'черный:слон', '2:g'=>'черный:король', '7:f'=>'белый:конь',
+    '8:g'=>'белый:слон', '8:h'=>'белый:король'));
 
 echo $result===null? "null": $result;
 
 function whoIsWinner(array $chessFigures)
 {
-
 	//обрабатываем входной массив
 	foreach($chessFigures as $arrayKey => $arrayValue)
 	{
 		$chessFigures[substr($arrayKey,0,2).(string)getCoordNumberFromCharacter
-					(substr($arrayKey,2,1))]=explode(":",$arrayValue);
+            (substr($arrayKey,2,1))]=
+            array_combine(array('color', 'figure'), explode(":",$arrayValue));
 		
 		unset($chessFigures[$arrayKey]);
 	}
 	
-	//var_dump($chessFigures);
-	
-	$coordinatesOfKingUnderAttack;
+	$coordinatesOfKingUnderAttack=null;
 
 	//ищем координаты фигуры, на которую поставили шах
 	foreach($chessFigures as $arrayKey => $arrayValue)
 	{
-			if(areFigureAttackedFromElephant($arrayKey, $chessFigures, $arrayValue[0]))
-			{
-				$coordinatesOfKingUnderAttack=explode(':',$arrayKey);
-				break;
-			}
-		   
+	    if($arrayValue['figure']==='король')
+	    {
+            if (areFigureAttackedFromElephant($arrayKey, $chessFigures, $arrayValue['color']))
+            {
+                $coordinatesOfKingUnderAttack = array_combine(array('coordY', 'coordX')
+                    ,explode(":",$arrayKey));
+                break;
+            }
+        }
 	}
-	
-	//var_dump($coordinatesOfKingUnderAttack);
 
-	
-	//проверка приведений типов
-	//var_dump((string)((int)$coordinatesOfKingUnderAttack[0]-1));
-	//var_dump((int)$coordinatesOfKingUnderAttack[0]-1);
-	
 	//если нет короля, на которого поставили шах, возвращаем null
 	if(empty($coordinatesOfKingUnderAttack))
 		return null;
 	
 	
 	//выношу цвет фигуры, на которую поставили шах
-	$kingColor=$chessFigures[($coordinatesOfKingUnderAttack[0].":".
-						   $coordinatesOfKingUnderAttack[1])][0];
+	$kingColor=$chessFigures[getCoordinateString
+        ($coordinatesOfKingUnderAttack['coordY']
+        ,$coordinatesOfKingUnderAttack['coordX'])]['color'];
 
 	//проверка, что при любом ходе короля, он умирает
-	if(areFigureAttackedFromElephant((string)((int)$coordinatesOfKingUnderAttack[0]-1).':'. 
-	  	$coordinatesOfKingUnderAttack[1],$chessFigures,$kingColor))
+	if(areFigureAttackedFromElephant(getCoordinateString
+        ((string)((int)$coordinatesOfKingUnderAttack['coordY']-1)
+            ,$coordinatesOfKingUnderAttack['coordX']),$chessFigures,$kingColor))
 	{
-		if(areFigureAttackedFromElephant((string)((int)$coordinatesOfKingUnderAttack[0]-1).':'. 
-		(string)((int)$coordinatesOfKingUnderAttack[1]-1),$chessFigures,$kingColor))
+		if(areFigureAttackedFromElephant(getCoordinateString
+            ((string)((int)$coordinatesOfKingUnderAttack['coordY']-1)
+            ,(string)((int)$coordinatesOfKingUnderAttack['coordX']-1))
+            ,$chessFigures,$kingColor))
 		{
 			return $kingColor==='белый'? 'черный': 'белый';
 		}
@@ -96,8 +94,7 @@ function areFigureAttackedFromElephant(string $figureUnderAttackCoordinates,arra
 	foreach($chessFigures as $arrayKey => $arrayValue)
 	{
 		//так как в рассматриваемом случае атакуют только слоны, проверяем их радиус атаки
-		//считается ли 'a'++ неявным преобразованием?
-		if($arrayValue[0]!== $figureColor && $arrayValue[1]==='слон')
+		if($arrayValue['color']!== $figureColor && $arrayValue['figure']==='слон')
 		{
 			$coordY=((int)substr($arrayKey,0,1))+1;
 			$coordX=((int)substr($arrayKey,2,1))+1;
@@ -117,4 +114,9 @@ function areFigureAttackedFromElephant(string $figureUnderAttackCoordinates,arra
 		}
 	}
 	return false;
+}
+
+function getCoordinateString(string $coordY, string $coordX)
+{
+    return $coordY.':'.$coordX;
 }
