cb7b3ae88a1331b19e1e0313cc9c7a22292e7c4e
Drozdov96 <drozdov.na96@gmail.com>
2018-11-10 18:51:23 +0400

    Add sending session data to the application.

--- task6/index.php
+++ task6/index.php
@@ -1,7 +1,16 @@
 <?php
 
+session_start();
+echo $_SESSION['player_one_field'];
+echo $_SESSION['player_two_field'];
+echo 1;
 //use sea_battle\Application;
 
 $app= new Application;
 
-$app->run();
+if(empty($_SESSION['player_one_field']) && empty($_SESSION['player_two_field']))
+{
+    $app->run();
+}else{
+    echo "s";
+}
