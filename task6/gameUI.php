82b76d522be8409dc4e9fc552ade5a270fecd08e
Drozdov96 <drozdov.na96@gmail.com>
2018-11-11 18:42:22 +0400

    Stuck on transfer data from view to model.

--- task6/gameUI.php
+++ task6/gameUI.php
@@ -14,7 +14,7 @@ for ($i = 1; $i <= 10; $i++)
 {
     for ($j = 1; $j <= 10; $j++)
     {
-        echo "<a href=\"index.php?x=${i}&y=${j}\" > </a>";
+        echo "<a href=\"index.php?x=${i}&y=${j}\" >as </a>";
     }
     echo "<br>";
 }
--- task6/index.php
+++ task6/index.php
@@ -1,13 +1,22 @@
 <?php
 
+define('SEABATTLE',$_SERVER['DOCUMENT_ROOT'].'/task6/sea_battle/');
+spl_autoload_register('autoload');
+function autoload($class)
+{
+    $file = SEABATTLE.$class.'.php';
+    if (is_readable($file))
+        include_once($file);
+}
+
 session_start();
-//echo $_SESSION['player_one_field'];
-//echo $_SESSION['player_two_field'];
 
 //use sea_battle\Application;
 
 if(!isset($app))
+{
     $app= new Application;
+}
 
 if(!empty($_GET['x']) && !empty($_GET['y']))
 {
